16bb9e30d212c8c46ad41eeaa3ac8e22c6e8a82d
mhcchm <marcoshcastello@usp.br>
2022-07-28 23:04:45 -0300

    Adiciona funções da classe Vector2D.
    Signed-off-by: mhcchm <marcoshcastello@usp.br>

--- app/src/main/kotlin/galaxyraiders/core/physics/Vector2D.kt
+++ app/src/main/kotlin/galaxyraiders/core/physics/Vector2D.kt
@@ -2,6 +2,12 @@
 package galaxyraiders.core.physics
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import kotlin.math.sqrt
+import kotlin.math.atan2
+import kotlin.math.PI
+import kotlin.math.abs
+import kotlin.math.cos
+
 
 @JsonIgnoreProperties("unit", "normal", "degree", "magnitude")
 data class Vector2D(val dx: Double, val dy: Double) {
@@ -10,57 +16,94 @@ data class Vector2D(val dx: Double, val dy: Double) {
   }
 
   val magnitude: Double
-    get() = INVALID_DOUBLE
+    get() = sqrt(dx * dx + dy * dy)
 
   val radiant: Double
-    get() = INVALID_DOUBLE
+    get() = atan2(dy, dx)
 
   val degree: Double
-    get() = INVALID_DOUBLE
+    get() = Math.toDegrees(this.radiant)
 
   val unit: Vector2D
-    get() = INVALID_VECTOR
+    get() = Vector2D(dx / this.magnitude, dy / this.magnitude)
 
   val normal: Vector2D
-    get() = INVALID_VECTOR
+    get() = Vector2D(this.unit.dy, -1 * this.unit.dx )
 
   operator fun times(scalar: Double): Vector2D {
-    return INVALID_VECTOR
+
+    return Vector2D(dx * scalar, dy * scalar)
   }
 
   operator fun div(scalar: Double): Vector2D {
-    return INVALID_VECTOR
+    val novoVetor: Vector2D = Vector2D(this.dx / scalar, this.dy / scalar)
+    return novoVetor
   }
 
   operator fun times(v: Vector2D): Double {
-    return INVALID_DOUBLE
+    val result: Double = dx * v.dx + dy * v.dy
+    return result
   }
 
   operator fun plus(v: Vector2D): Vector2D {
-    return INVALID_VECTOR
+    //soma de vetores
+    val novoVetor: Vector2D = Vector2D(this.dx + v.dx, this.dy + v.dy)
+    return novoVetor
   }
 
   operator fun plus(p: Point2D): Point2D {
-    return INVALID_POINT
+    val novoPonto: Point2D = Point2D(this.dx + p.x, this.dy + p.y)
+    return novoPonto
   }
 
   operator fun unaryMinus(): Vector2D {
-    return INVALID_VECTOR
+    val novoVetor: Vector2D = Vector2D(this.dx * -1, this.dy * -1)
+    return novoVetor
   }
 
   operator fun minus(v: Vector2D): Vector2D {
-    return INVALID_VECTOR
+    val novoVetor: Vector2D = Vector2D(this.dx - v.dx, this.dy - v.dy)
+    return novoVetor
   }
 
   fun scalarProject(target: Vector2D): Double {
-    return INVALID_DOUBLE
+    val ang: Double
+    val angThis: Double
+    val angTarget: Double
+
+    if (target.dx == 0.0 || target.dy == 0.0){
+      val valor: Double
+      //colocamos dois if's encaixados pois o linter recomenda que a quantidade de returns
+      //de uma função não exceda dois.
+      if(target.dx == 0.0){
+        valor = this.dy
+      }else{
+        valor = this.dx
+      }
+      return valor
+    }
+
+    if (this.radiant < 0){
+      angThis = this.radiant + 2 * PI
+    }else{
+      angThis = this.radiant
+    }
+    if (target.radiant < 0){
+      angTarget = target.radiant + 2 * PI
+    }else{
+      angTarget = target.radiant
+    }
+    ang = abs(angTarget - angThis)
+    return this.magnitude * cos(ang)
   }
 
   fun vectorProject(target: Vector2D): Vector2D {
-    return INVALID_VECTOR
+    val versor: Vector2D = target.unit
+    return versor.times(this.scalarProject(target))
   }
+
 }
 
 operator fun Double.times(v: Vector2D): Vector2D {
-  return INVALID_VECTOR
+  return Vector2D(this * v.dx, this * v.dy)
 }
